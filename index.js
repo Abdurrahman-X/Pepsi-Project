fdec0d7959b36e53adfa6053e7f333022b261d67
Abdurrahman_X <abolajiabdurrahman@gmail.com>
2020-12-28 02:23:30 +0100

    LO

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// const products = document.querySelector(".products");
-// const clip = document.querySelector(".clip-path");
-// //console.log(babes);
+const products = document.querySelector(".products");
+const clip = document.querySelector(".clip-path");
+//console.log(clip);
 
 
-// function imageChange(something) {
-//      document.querySelector(".pepsi").src = something;
+function imageChange(something) {
+     document.querySelector(".pepsi").src = something;
+}
+
+function changeClipColor(color) {
+    const circle = document.querySelector( ".clip-path");
+    circle.style.background = color;
+}
+
+function changeButtonColor(color) {
+    const button = document.querySelector( ".products");
+    button.style.background = color;
+}
+
+// function changeBackground(color) {
+//     const container = document.querySelector('.container');
+//     container.style.background = color;
 // }
-
-// function changeClipColor(color) {
-//     const circle = document.querySelector( ".clip-path");
-//     circle.style.background = color;
-// }
-
-// function changeButtonColor(color) {
-//     const button = document.querySelector( ".products");
-//     button.style.background = color;
-// }
-
-// // function changeBackground(color) {
-// //     const container = document.querySelector('.container');
-// //     container.style.background = color;
-// // }
